45e759955fab1b4144dcae3854b9a3ba6bfdd020
FeDroid74 <fedroid74@gmail.com>
2025-03-13 09:45:20 +0300

    Каталог v2

--- catalog.php
+++ catalog.php
@@ -30,13 +30,13 @@ require './server/products.php';
                 <aside class="filter">
                     <h2 class="filter-title">Бренды</h2>
                     <ul class="filter-list">
-                        <li><input type="checkbox"> adidas Originals 7</li>
-                        <li><input type="checkbox"> ASICS 4</li>
-                        <li><input type="checkbox"> Carhartt WIP 1</li>
-                        <li><input type="checkbox"> Converse 25</li>
-                        <li><input type="checkbox"> Diadora 4</li>
-                        <li><input type="checkbox"> Hijack Sandals 6</li>
-                        <li><a href="#">еще 12</a></li>
+                        <li><label><input type="checkbox"> adidas Originals 7</label></li>
+                        <li><label><input type="checkbox"> ASICS 4</label></li>
+                        <li><label><input type="checkbox"> Carhartt WIP 1</label></li>
+                        <li><label><input type="checkbox"> Converse 25</label></li>
+                        <li><label><input type="checkbox"> Diadora 4</label></li>
+                        <li><label><input type="checkbox"> Hijack Sandals 6</label></li>
+                        <li><a href="#" class="more-brands">еще 12</a></li>
                     </ul>
 
                     <h2 class="filter-title">Пол</h2>
@@ -62,9 +62,14 @@ require './server/products.php';
 
                     <h2 class="filter-title">Размер EUR</h2>
                     <div class="size-grid">
-                        <button>38</button> <button>39</button> <button>40</button>
-                        <button>41</button> <button>42</button> <button>43</button>
-                        <button>44</button> <button>45</button> <button>46</button>
+                        <button>45-46</button> <button>30</button> <button>35</button> <button>35.5</button> <button>36</button>
+                        <button>36 2/3</button> <button>36.5</button> <button>37</button> <button>37.5</button> <button>38</button>
+                        <button>38.5</button> <button>39</button> <button>39.5</button> <button>40</button> <button>40 2/3</button>
+                        <button>40.5</button> <button>41</button> <button>41 1/3</button> <button>41-42</button> <button>41.5</button>
+                        <button>42</button> <button>42 1/3</button> <button>42 2/3</button> <button>42.5</button> <button>43</button>
+                        <button>43-44</button> <button>43 2/3</button> <button>43 1/3</button> <button>44</button> <button>44 2/3</button>
+                        <button>44.5</button> <button>45</button> <button>45.5</button> <button>46</button> <button>46.5</button>
+                        <button>47</button> <button>47.5</button> <button>48.5</button> <button>49.5</button> <button>50.5</button>
                     </div>
                 </aside>
 
